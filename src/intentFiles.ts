717f7a48801b62d319066f306e22bba93a8fa82d
Tiger Oakes <tigeroakes@gmail.com>
2017-03-19 00:36:11 -0700

    Changed keys to letters

--- src/intentFiles.ts
+++ src/intentFiles.ts
@@ -1,18 +1,21 @@
 import { getAllActions } from './game';
+import { PASS_THROUGH } from './parseStory';
 
-const actions = Array.from(getAllActions())
-	.filter(([id]) => parseInt(id, 10) < 1000);
+const actions = getAllActions();
 const prefix = process.argv[3] || '';
 
 if (process.argv[2] === 'utter') {
-	actions.forEach(([,action]) => {
+	actions.forEach(action => {
+		if (action.message === PASS_THROUGH) return;
 		console.log(`${prefix}${action.node} ${action.message}`);
 		console.log('');
 	})
 } else if (process.argv[2] === 'json') {
-	const json = {
-		intents: actions.map(([key]) => ({ intent: `${prefix}${key}` })),
-	};
+	const json: any = { intents: [] };
+	actions.forEach(({ message }, key) => {
+		if (message === PASS_THROUGH) return;
+		json.intents.push({ intent: `${prefix}${key}` });
+	})
 	console.log(JSON.stringify(json));
 } else {
 	console.error('Please use either "utter" or "json"');
