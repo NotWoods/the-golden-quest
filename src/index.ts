e042f4c1f1fd7b61d364ac5321aa6c2064e8dbe2
Janet Chen <janetchen906@gmail.com>
2017-03-18 17:52:45 -0700

    Created shell for states

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as Alexa from 'alexa-sdk';
 import { readSampleUtterances } from './fs';
 
 const data = require('../speechAssets/IntentSchema.json');
-var text = await readSampleUtterances();
+//var text = await readSampleUtterances();
 
 const APP_ID = ''; //TODO
 
@@ -64,66 +64,14 @@ var newSessionHandlers = {
     "AMAZON.StartOverIntent": function() {
         this.handler.state = GAME_STATES.START;
         this.emitWithState("StartGame", true);
-    },
-   /* "AMAZON.HelpIntent": function() {
-        this.handler.state = GAME_STATES.HELP;
-        this.emitWithState("helpTheUser", true);
-    },
-    "Unhandled": function () {
-        var speechOutput = this.t("START_UNHANDLED");
-        this.emit(":ask", speechOutput, speechOutput); */
     }
 };
 
-var helpStateHandlers = Alexa.CreateStateHandler(GAME_STATES.HELP, {
-    "helpTheUser": function (newGame) {
-       // var askMessage = newGame ? this.t("ASK_MESSAGE_START") : this.t("REPEAT_QUESTION_MESSAGE") + this.t("STOP_MESSAGE");
-        var speechOutput = this.t("HELP_MESSAGE", GAME_LENGTH) + askMessage;
-        var repromptText = this.t("HELP_REPROMPT") + askMessage;
-        this.emit(":ask", speechOutput, repromptText);
-    },
-    "AMAZON.StartOverIntent": function () {
-        this.handler.state = GAME_STATES.START;
-        this.emitWithState("StartGame", false);
-    },
-   /* "AMAZON.RepeatIntent": function () {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    }, 
-    "AMAZON.HelpIntent": function() {
-        var newGame = (this.attributes["speechOutput"] && this.attributes["repromptText"]) ? false : true;
-        this.emitWithState("helpTheUser", newGame);
-    }, 
-    "AMAZON.YesIntent": function() {
-        if (this.attributes["speechOutput"] && this.attributes["repromptText"]) {
-            this.handler.state = GAME_STATES.TRIVIA;
-            this.emitWithState("AMAZON.RepeatIntent");
-        } else {
-            this.handler.state = GAME_STATES.START;
-            this.emitWithState("StartGame", false);
-        }
-    },
-    async "AMAZON.NoIntent"() {
-        var speechOutput = this.t("NO_MESSAGE");
-        this.emit(":tell", speechOutput);
-    },
-    "AMAZON.StopIntent": function () {
-        var speechOutput = this.t("STOP_MESSAGE");
-        this.emit(":ask", speechOutput, speechOutput);
-    },
-    "AMAZON.CancelIntent": function () {
-        this.emit(":tell", this.t("CANCEL_MESSAGE"));
-    },
-    "Unhandled": function () {
-        var speechOutput = this.t("HELP_UNHANDLED");
-        this.emit(":ask", speechOutput, speechOutput);
-    }, 
-    */
-    "SessionEndedRequest": function () {
-        console.log("Session ended in help state: " + this.event.request.reason);
+var startStateHandlers = Alexa.CreateStateHandler(GAME_STATES.START, {
+    "StartGame": function (newGame) {
+        var speechOutput = newGame ? this.t("NEW_GAME_MESSAGE", this.t("GAME_NAME")) + this.t("WELCOME_MESSAGE"): "";
     }
 });
-
 
 
 function hi() {
